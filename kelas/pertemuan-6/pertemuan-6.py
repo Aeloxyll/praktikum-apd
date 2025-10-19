8eb776a4c87ce8151d6baa829f2ebd400a6e928c
Hammam <hssyamil7@gmail.com>
2025-10-19 20:53:02 +0800

    update

---
+++ kelas/pertemuan-6/pertemuan-6.py
@@ -0,0 +1,7 @@
+angka_ganjil = {1, 3, 5, 7, 9}
+angka_random = {2, 3, 5, 7, 11}
+print("menambahkan 11 ke dalam set angka_ganjil")
+angka_ganjil.add(11)
+
+gabung = union(set1=angka_ganjil, set2=angka_random)
+print(gabung)
